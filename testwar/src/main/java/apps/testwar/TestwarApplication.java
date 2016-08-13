60cac7c975d274c117d4c06c734dbab7274c8149
henrytu <tutrunghieu@gmail.com>
2016-08-13 13:56:15 +0700

    Add project

---
+++ testwar/src/main/java/apps/testwar/TestwarApplication.java
@@ -0,0 +1,12 @@
+package apps.testwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestwarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestwarApplication.class, args);
+	}
+}
